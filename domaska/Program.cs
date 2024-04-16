20a0822e95a6e032b67d6bd46c38ccbff8f56de4
askerov.ehmed@mail.ru <askerov.ehmed@mail.ru>
2024-04-16 21:05:52 +0200

    домашка

---
+++ domaska/Program.cs
@@ -0,0 +1,38 @@
+﻿// 10-1  Написать программу, которая запрашивает у пользователя значение
+//радиуса окружности и выводит на экран ее длину и площадь,
+//Console.Write("Ввидите площадь круга:");
+//Double s1 = int.Parse(Console.ReadLine());
+//Console.Write("Ввидите длину и площадь :");
+//Double s2 = int.Parse(Console.ReadLine());
+//Double R = Math.Sqrt(s1 / Math.PI);
+//Double a = Math.Sqrt(s2);
+//if (2 * R <= a) Console.WriteLine("крук умечтится в длину");
+//else Console.WriteLine("крук не умечтится в длину");
+//if (2 * a * a <= 4 * R * R) Console.WriteLine("крук умечтится в длину");
+//else Console.WriteLine("крук не умечтится в длину");
+
+
+
+
+// 10-2 Написать программу, которая запрашивает у пользователя значение
+//катета и гипотенузы прямоугольного треугольника, а затем выводит на
+//экран значение второго катета, вычисленное с помощью методов класса
+//Math
+
+//Console.Write("Введите первый катет ");
+//double a = Convert.ToDouble(Console.ReadLine());
+//if (a < 0)
+//{
+//    Console.WriteLine("Вводите только положительные числа!");
+//}
+//Console.Write("Введите второй катет ");
+//double b = Convert.ToDouble(Console.ReadLine());
+//if (b < 0)
+//{
+//    Console.WriteLine("Вводите только положительные числа!");
+//}
+
+//Console.Write("Гипотенуза равна " + Math.Sqrt(a * a + b * b));
+//Console.ReadLine();
+
+//Console.ReadKey();
