694a2b5ee17890515e5c10cdd3622e23f95a4f9a
askerov.ehmed@mail.ru <askerov.ehmed@mail.ru>
2024-04-30 21:51:49 +0200

    lesson 12.1

---
+++ domaska12-1/Program.cs
@@ -0,0 +1,30 @@
+﻿////Домашнее задание. Тема 12. Урок 1. Операторы
+////ветвления Switch/ Case.
+////Калькулятор: пользователь может выбрать операцию (сложение,
+////вычитание, умножение, деление), ввести два числа и получить результат
+
+
+Random random = new Random();
+
+Console.Write("Введите первое число:");
+int n = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число:");
+int m = int.Parse(Console.ReadLine());
+switch (n)
+{
+
+    case 1:
+        {
+            Console.Write("Введите Ответ:");
+                break;
+        }
+        case 2:
+        {
+        Console.Write("Введите второе число:"); break;
+
+        }
+
+
+
+}
+Console.WriteLine($"сложение  {n+m} || вычитание  {n-m} || умножение  {n*m} || деление {n/m}");
