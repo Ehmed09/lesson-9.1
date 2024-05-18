54797a917671ab0842c89f25fde3a04d1027162e
askerov.ehmed@mail.ru <askerov.ehmed@mail.ru>
2024-05-18 14:03:23 +0200

    lesson 19-1

---
+++ domaska 15-1  15-2/Program.cs
@@ -0,0 +1,21 @@
+﻿//Random random = new Random();
+string[] names = new string[10]  ;
+string name1 = "освоении";
+string name2 = "“остальное";
+string name3 = "по плечу";
+string name4 = "сложный";
+string name5 = "но если";
+string name6 = "программирования";
+string name7 = "многие бросают";
+string name8 = "остальное будет";
+string name9 = "“освоить их,";
+string name10 = "этап в";
+
+//string[] names = new string[10];
+//int i = 0;
+for (int i = 0; i < names.Length; i++)
+{
+    Console.Write(names[i]);
+}
+
+Console.WriteLine();
